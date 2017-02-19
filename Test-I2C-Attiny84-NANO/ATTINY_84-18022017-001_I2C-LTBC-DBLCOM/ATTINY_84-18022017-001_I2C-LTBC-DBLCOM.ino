81ab57b44bc984a71196ab88efc9b26e5b644372
Almisuifre <alexandre.labure@gmail.com>
2017-02-19 17:47:13 +0100

    Création d'un test de communication bi-directionnel
    Création d'un test de communication bi-directionnel I2C avec 1 bouton et 1 led.

---
+++ Test-I2C-Attiny84-NANO/ATTINY_84-18022017-001_I2C-LTBC-DBLCOM/ATTINY_84-18022017-001_I2C-LTBC-DBLCOM.ino
@@ -0,0 +1,60 @@
+/*
+ * ATTINY 84
+ * 
+ * Pin Utilisés
+ *VCC -> 1    14 <- GND
+ */
+
+/* INCLUDE */
+#include <TinyWireS.h>
+
+/* DEFINE */
+#define LED 8
+#define BTN 1
+
+//int valBTN;
+boolean valBTN;
+boolean oldValBTN;
+byte returnBTN = 0;
+byte byteRcvd = 0;
+
+void setup()
+{
+  // put your setup code here, to run once:
+  TinyWireS.begin(0x00);  // Activation de l'I2C avec l'adresse 000|0000 soit 0x00 à 000|1111| 0x15
+
+  pinMode(LED, OUTPUT); //Led en sortie
+  pinMode(BTN, INPUT);  //Bouton en entrée
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  
+  if (TinyWireS.available())
+  {
+    byteRcvd = TinyWireS.receive(); //Data reçu
+
+    switch(byteRcvd)
+    {
+      case 0x01:
+        digitalWrite(LED, HIGH);
+        break;
+      case 0x02:
+        digitalWrite(LED, LOW);
+        break;
+    }
+
+    valBTN = digitalRead(BTN);
+    
+    if(valBTN != oldValBTN)
+    {
+      oldValBTN = valBTN;
+    }
+
+    returnBTN = oldValBTN; //Transforme en byte
+    TinyWireS.send(returnBTN);
+  }
+}
+
+
---
+++ Test-I2C-Attiny84-NANO/NANO-18022017-002-LTBC-TEST-DBLCOM/NANO-18022017-002-LTBC-TEST-DBLCOM.ino
@@ -0,0 +1,37 @@
+#include <Wire.h>
+
+byte data;
+
+void setup() {
+  // put your setup code here, to run once:
+  Wire.begin();
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  
+  // put your main code here, to run repeatedly:
+  Wire.beginTransmission(0x00); //Adresse esclave
+  Wire.write(0x01);
+  Wire.endTransmission();
+  delay(10);
+  Wire.beginTransmission(0x00); //Adresse esclave
+  Wire.write(0x02);
+  Wire.endTransmission();
+  delay(100);
+  
+  /*Wire.beginTransmission(0x00); //Adresse esclave
+  Wire.write(0x06); //Lancer état bouton
+  Wire.endTransmission();
+  delay(100);*/
+  
+  Wire.requestFrom(0x00, 1);  //Recevoir 1 byte depuis l'esclave
+  if(Wire.available())
+  {
+    data = Wire.read(); //Réception data
+    Serial.println(data);
+  }
+
+  delay(1);
+}
